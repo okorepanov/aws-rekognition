2316afe75ca9c14c40cc145aa68d923877976ec5
Korepanov Oleksandr <rskillkharkv@gmail.com>
2024-01-15 01:18:14 +0200

    Replace AWS_DEFAULT_REGION env variable with AWS_REGION

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 Aws.config[:credentials] = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-Aws.config[:region] = ENV['AWS_DEFAULT_REGION']
+Aws.config[:region] = ENV['AWS_REGION']
